0eca67fc25525a207ca54a0312ffc1b0337554a9
bnetobradley <barbaranetobradley@gmail.com>
2019-02-06 15:38:07 -0800

    sourcing nitrogen data and exploring species sampling options

--- code/species_lists.R
+++ code/species_lists.R
@@ -1,8 +1,9 @@
 ## This is code for exploring potential species sampling in a study of how morphology and metabolism are linked accross phylogeny in vascular plants
 library(tidyverse)
-devtools::install_github("ropenscilabs/datastorr")
-devtools::install_github("wcornwell/taxonlookup")
+#devtools::install_github("ropenscilabs/datastorr")
+#devtools::install_github("wcornwell/taxonlookup")
 library(taxonlookup)
+library(plyr)
 
 ubc <- read.csv("data/UBCBG_taxa.csv")
 ## 12937 rows of data
@@ -33,6 +34,11 @@ vand <- vand[!grepl("sp.", vand$NAME),]
 vand <- vand[!grepl("× ", vand$NAME),]
 vand <- vand[!grepl(" × ", vand$NAME),]
 vand <- vand[!grepl("\\+", vand$NAME),]
+vand <- vand[!grepl("var.", vand$NAME),]
+vand <- vand[!grepl("f.", vand$NAME),]
+vand <- vand[!grepl("Group", vand$NAME),]
+vand <- vand[!grepl("\\'", vand$NAME),]
+vand <- vand[!grepl("\\(", vand$NAME),]
 vand <- vand[!grepl("garden origin", vand$NATIVITY),]
 vand <- vand[!grepl("Garden Origin", vand$NATIVITY),]
 
@@ -46,4 +52,64 @@ vand_taxlist <- lookup_table(vand_splist, by_species = TRUE)
 all_taxa_available <- rbind(vand_taxlist, ubc_taxlist)
 count(all_taxa_available$family)
 count(all_taxa_available$group)
+count(all_taxa_available$order)
+all_taxa_available$binomial <- rownames(all_taxa_available)
 ## 194 families represented 
+
+
+## what trait data is available?
+library(BIEN)
+spbysp_leafn <- BIEN_trait_traitbyspecies(all_taxa_available$binomial, trait = "leaf nitrogen content per leaf area")
+sub_bien <- spbysp_leafn %>% select(scrubbed_species_binomial, trait_value, project_pi)
+colnames(sub_bien) <- c("taxa", "nitrogen", "source")
+## 180 species
+spbysp_leafa <-BIEN_trait_traitbyspecies(all_taxa_available$binomial, trait = "leaf area")
+## 363 species
+spbysp_leafl <-BIEN_trait_traitbyspecies(all_taxa_available$binomial, trait = "leaf life span")
+## 78 species
+
+scrubbed_tax_list <- lookup_table(unique(spbysp_leafn$scrubbed_species_binomial), by_species = TRUE)
+count(scrubbed_tax_list$group) ## angio = 130 // gymno = 29 // ferns = 21
+count(scrubbed_tax_list$family)
+
+##install.packages("taxize")
+#library(taxize)
+#classdat <- classification(all_taxa_available$binomial, db = "tropicos", callopts = FALSE, return_id = TRUE)
+print(all_taxa_available$binomial)
+
+## figuring out where else to pull data from
+try_scam <- read.csv("data/try_leafn_species_list.csv")
+try_scrubbed <- try_scam[(which(try_scam$AccSpeciesName %in% all_taxa_available$binomial)),]
+unique(try_scrubbed$AccSpeciesName)
+summary(try_scrubbed$Dataset)
+
+## largest datasets
+## reich-oleksyn global leaf n, p database ## downloaded 2019Feb04
+## topic ## not open access 2019Feb05
+## plant volatiles ## n data is not open access 2019Feb05
+## global 15N database ## not open access 2019Feb05
+## catalonian mediterranean forest trait db ## downloaded 2019Feb05
+
+reich_dat <- read.csv("data/reich_oleksyn_dataset.csv")
+reich_dat <- reich_dat[which(reich_dat$Species %in% all_taxa_available$binomial),]
+sub_reich <- reich_dat %>% select(Species, N..mg.g, References)
+colnames(sub_reich) <- c("taxa", "nitrogen", "source")
+## 153 taxa in reich dataset for N content
+
+brot <- read.csv("data/BROT2_dat.csv")
+brot <- brot %>% filter(Trait == "LNCm")
+brot <- brot[(which(brot$Taxon %in% all_taxa_available$binomial)),]
+sub_brot <- brot %>% select(Taxon, Data, SourceID)
+colnames(sub_brot) <- c("taxa", "nitrogen", "source")
+
+glopnet <- read.csv("data/glopnet.csv")
+glopnet <- glopnet[which(glopnet$Species %in% all_taxa_available$binomial),]
+sub_glop <- glopnet %>% select(Species, log.Nmass, Dataset)
+colnames(sub_glop) <- c("taxa", "nitrogen", "source")
+
+all_data_available <- rbind(sub_glop, sub_reich, sub_brot, sub_bien)
+## data available for 272 taxa
+## nitrogen values not in same units - check for log transformation in glopnet dataset
+
+taxonlist <- as.character(unique(all_data_available$taxa))
+taxonlist <- lookup_table(taxonlist, by_species = TRUE)
