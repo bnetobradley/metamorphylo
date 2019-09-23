b25d2664449b985823b1cef66f354b522ac7bcb5
bnetobradley <barbaranetobradley@gmail.com>
2019-09-23 10:58:53 -1000

    function for recalculating licor outputs updated to cover variables E:EBSum; includes message about constants from header

--- code/recalculate_licor_racir.R
+++ code/recalculate_licor_racir.R
@@ -4,28 +4,34 @@ library("tidyverse")
 
 # calculate extra leaf related metrics which scale from basic set of measurements
 recalc_licor68 <- . %>%
-  select(-(A:TleafCnd)) %>%
+  select(-(A:EBSum)) %>%
   mutate(
     E = Flow*CorrFact*(H2O_s-H2O_r)/(100*S*(1000-CorrFact*H2O_s)),
     A = Flow*CorrFact*(CO2_r-CO2_s*(1000-CorrFact*H2O_r)/(1000-CorrFact*H2O_s))/(100*S),
     Ca = CO2_s - ifelse(CorrFact>1, A*S*100/(Fan*Fan_speed), 0),
-    Ci = CO2_s - ifelse(CorrFact>1, A*S*100/(Fan*Fan_speed), 0)
-    #df$Pci <- df$Ci*(df$Pa+df$ΔPcham)/1000
-    #df$Pca <- (df$CO2_s - IF(df$CorrFact>1, df$A*df$S*100/(df$Fan*df$Fan_speed), 0))*(df$Pa+df$ΔPcham)/1000
-    #df$gsw <- 2/((1/Q20-1/P20)+SIGN(Q20)*SQRT((1/Q20-1/P20)*(1/Q20-1/P20) + 4*AR20/((AR20+1)*(AR20+1))*(2*1/Q20*1/P20-1/P20*1/P20)))
-    #df$gbw <- AG20+AF20*df$S+AE20*df$S*df$S
-    #df$gtw <- I20*(1000-(1000*0.61365*EXP(17.502*U20/(240.97+U20))/(df$Pa+df$ΔPcham)+df$H2O_s)/2)/(1000*0.61365*EXP(17.502*U20/(240.97+U20))/(df$Pa+df$ΔPcham)-df$H2O_s)
-    #df$gtc <- 1/((AR20+1)/(O20/1.6)+1/(P20/1.37)) + AR20/((AR20+1)/(O20/1.6) + AR20/(P20/1.37))
-    #df$Rabs <- (AN20*AP20)
-    #df$TleafEB <- (BB20+(S20+2*0.95*0.0000000567*(((BB20+$B$7)+273)^4-(BB20+273)^4)-44100*I20)/(1.84*29.3*P20+8*0.95*0.0000000567*(BB20+273)^3))
-    #df$TleafCnd <- ($C$7*BC20+$D$7*BD20+$E$7*T20)
-    #df$SVPleaf <- 0.61365*EXP(17.502*U20/(240.97+U20))
-    #df$RHcham <- (X20/Y20*100)
-    #df$VPcham <- df$H2O_s*(df$Pa+df$ΔPcham)/1000
-    #df$SVPcham <- 0.61365*EXP(17.502*BB20/(240.97+BB20))
-    #df$VPDleaf <- (V20-df$H2O_s*(df$Pa+df$ΔPcham)/1000)
-    #df$LatHFlux <- (-I20*44100)
-    #df$SenHFlux <- 2*29.3*P20*0.92*(BB20-U20)
-    #df$NetTherm <- 2*0.95*0.0000000567*(((BB20+$B$7)+273)^4-(U20+273)^4)
-    #df$EBSum <- S20+AC20+AA20+AB20
+    gbw = blfa_3+blfa_2*S+blfa_1*S*S,
+    Rabs = (Qabs*convert),
+    TleafEB = (Tair+(Rabs+2*0.95*0.0000000567*(((Tair+0)+273)^4-(Tair+273)^4)-44100*E)/(1.84*29.3*gbw+8*0.95*0.0000000567*(Tair+273)^3)),
+    ## n.b. deltaTw set at 0
+    TleafCnd = (1*Tleaf+0*Tleaf2+0*TleafEB),
+    ## n.b. fT1, fT2 and fTeb set at 1, 0, 0
+    gtw = E*(1000-(1000*0.61365*exp(17.502*TleafCnd/(240.97+TleafCnd))/(Pa+ΔPcham)+H2O_s)/2)/(1000*0.61365*exp(17.502*TleafCnd/(240.97+TleafCnd))/(Pa+ΔPcham)-H2O_s),
+    gsw = 2/((1/gtw-1/gbw)+sign(gtw)*sqrt((1/gtw-1/gbw)*(1/gtw-1/gbw) + 4*K/((K+1)*(K+1))*(2*1/gtw*1/gbw-1/gbw*1/gbw))),
+     gtc = 1/((K+1)/(gsw/1.6)+1/(gbw/1.37)) + K/((K+1)/(gsw/1.6) + K/(gbw/1.37)),
+     Ci = ((gtc-E/2)*Ca-A)/(gtc+E/2),
+    Pci = Ci*(Pa+ΔPcham)/1000,
+    Pca = (CO2_s - ifelse(CorrFact>1, A*S*100/(Fan*Fan_speed), 0))*(Pa+ΔPcham)/1000,
+     SVPleaf = 0.61365*exp(17.502*TleafCnd/(240.97+TleafCnd)),
+   VPcham = H2O_s*(Pa+ΔPcham)/1000,
+   SVPcham = 0.61365*exp(17.502*Tair/(240.97+Tair)),
+   RHcham = (VPcham/SVPcham*100),
+    VPDleaf = (SVPleaf-H2O_s*(Pa+ΔPcham)/1000),
+    LatHFlux = (-E*44100),
+    SenHFlux = 2*29.3*gbw*0.92*(Tair-TleafCnd),
+    NetTherm = 2*0.95*0.0000000567*(((Tair+0)+273)^4-(TleafCnd+273)^4),
+    ## n.b. deltaTw set at 0
+    EBSum = Rabs+NetTherm+LatHFlux+SenHFlux,
+   
+   message("Constant values printed in the header of the licor output file are ignored in this recalculation.
+This means that the values for deltaTw (0), fT1 (1), fT2 (0) or fTeb (0) are set as denoted in brackets.")
   )
